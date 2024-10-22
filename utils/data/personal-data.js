a537c55ed2c002a32f5069794da003bac414d501
Sonu Sharma <sonusharma30.09.2004@gmail.com>
2024-10-22 08:14:55 +0530

    some change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Sonu Sharma",
   profile: '/profile.png',
   designation: "Mern Stack Developer",
-  description: "My name is Sonu Sharma. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I'm Sonu Sharma, a passionate MERN stack developer with a strong focus on JavaScript. I enjoy learning new technologies and solving problems through web development. Skilled in building full-stack applications using MongoDB, Express, React, and Node.js, I'm always eager to make the web more accessible. Open to job opportunities that match my skills and interests.",
   email: 'sonusharma30.09.2004@gmail.com',
   phone: '+91 8209950683',
   address: 'Jaipur',
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -4,24 +4,24 @@ export const projectsData = [
         name: 'iShop Website',
         description: "I developed the iShop e-commerce website using React and Node.js, featuring secure user authentication and a payment gateway for smooth transactions. With Redux for state management and Tailwind CSS for a responsive, modern UI, the backend is powered by Express and MongoDB to store user and product data. This project showcases my ability to build functional, full-stack applications.",
         image: '/ishop.jpg',
-        githubLink:"sdf",
-        demoLink:"fds"
+        githubLink:"https://github.com/Ssp9917/i-shop",
+        demoLink:"https://ishopeshopping.vercel.app/"
     },
     {
         id: 2,
         name: 'Restaurant Website',
         description: 'I developed a restaurant website using the MERN stack, featuring an intuitive interface for browsing menus, placing orders, and making reservations. It includes real-time order tracking and an admin panel for managing menu items and reservations. The site is fully responsive, ensuring a seamless experience across devices, showcasing my ability to build efficient and user-friendly full-stack applications.',
         image: '/cafe.jpg',
-        githubLink:"sdf",
-        demoLink:"das"
+        githubLink:'https://github.com/Ssp9917/cafe-website',
+        demoLink:"https://cafe-website-sable.vercel.app/"
     },
     {
         id: 3,
         name: 'School Dashboard',
         description: 'I developed a school dashboard using Next.js, featuring role-based access for students, teachers, and administrators. The dashboard allows users to manage schedules, track performance, and organize tasks. With a fully responsive design, it ensures seamless navigation across devices, highlighting my skills in creating scalable and efficient full-stack applications.  ',
         image: '/school.jpg',
-        githubLink:"dsa",
-        demoLink:"sda"
+        githubLink:"https://github.com/Ssp9917/next-dashboard-ui",
+        demoLink:"https://next-dashboard-ui-murex.vercel.app/admin"
     },
 ];
 
