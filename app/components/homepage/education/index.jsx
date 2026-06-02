4ac6f8d80c01244b5ca1d39a1638770df7621095
ssp20460 <ssp20460@gmail.com>
2026-06-02 08:35:31 +0530

    fix: resolve Lottie document prerender error and add Vercel Speed Insights

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 import { educations as defaultEducations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
-import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 import lottieFile from '/public/lottie/study.json';
+import dynamic from "next/dynamic";
+
+const AnimationLottie = dynamic(() => import("../../helper/animation-lottie"), { ssr: false });
 
 function Education({ educations = defaultEducations }) {
   return (
